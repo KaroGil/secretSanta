0631aa0d06e282d44dd3356de0601afd16d3e286
KaroGil <karolinkagil@gmail.com>
2024-11-01 16:51:04 +0100

    add prettier and lint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "next/core-web-vitals", // Recommended Next.js ESLint rules
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    // Customize any rules you want here
+  },
+};
