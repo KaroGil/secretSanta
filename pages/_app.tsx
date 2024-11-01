7de5f7ad37e46b38b4e9ad835891a78ec0e79626
KaroGil <karolinkagil@gmail.com>
2024-11-01 22:29:28 +0100

    icons behind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,12 @@ import type { AppProps } from 'next/app';
 import { AnimatedIcons } from '../pages/components/AnimatedIcons';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const isDecember = new Date().getMonth() === 10;
   return (
     <>
-      {isDecember && <AnimatedIcons n={50} />}
-      <Component {...pageProps} />;
+      <AnimatedIcons n={50} />
+      <div className="main-content">
+        <Component {...pageProps} />;
+      </div>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import MainCard from './components/MainCard';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
+    <div className="min-h-screen flex flex-col items-center justify-center">
       <Head>
         <title>Secrete santa picker</title>
         <link rel="icon" href="/favicon.ico" />
