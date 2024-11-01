5e4e2bc964066188e0197d14ba5ba491350f981f
KaroGil <karolinkagil@gmail.com>
2024-11-01 22:34:20 +0100

    add stoopid footer

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -3,14 +3,9 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="flex items-center justify-center w-full h-16 border-t mt-8">
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center"
-      >
-        Powered by <img src="/vercel.svg" alt="Vercel" className="h-4 ml-2" />
-      </a>
+      <p className="text-sm">
+        <i>@KaroGil &notin; sane</i>
+      </p>
     </footer>
   );
 }
